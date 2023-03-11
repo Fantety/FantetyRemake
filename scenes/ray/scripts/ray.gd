c79a536f1e8cdbe2dc5af0c42052a1c83b6f301d
Fantety <fantety@outlook.com>
2023-03-11 17:16:38 +0800

    feat: bedroom mini game

--- scenes/ray/scripts/ray.gd
+++ scenes/ray/scripts/ray.gd
@@ -12,13 +12,13 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" and Common.player_permission <= 0:
 		CommonSignal.emit_signal("call_player_enter_ray")
 		pass
 	pass # Replace with function body.
 
 
 func _on_area_2d_body_exited(body):
-	if body.name == "Player":
+	if body.name == "Player" and Common.player_permission <= 0:
 		pass
 	pass # Replace with function body.
